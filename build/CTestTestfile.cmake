04ba117656f0c0b1c42f79852ee5fec3966770fc
Abhishek Kumar <abhishek.en@gmail.com>
2024-02-19 09:15:22 +0530

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abs/processManager
-# Build directory: /home/abs/processManager/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
