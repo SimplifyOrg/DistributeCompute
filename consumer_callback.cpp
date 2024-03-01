5360c128ddaf06b3f71310a20d58ef3b787553fe
Abhishek Kumar <abhishek.en@gmail.com>
2024-03-01 10:28:39 +0530

    Basic process launch code

--- consumer_callback.cpp
+++ consumer_callback.cpp
@@ -3,9 +3,13 @@
 #include "uuid_generator.h"
 #include <thread>
 #include <bsl_string.h>
+#include <bsl_memory.h>
 #include <string>
 #include <boost/process/search_path.hpp>
+#include <boost/filesystem.hpp>
 #include "config.h"
+#include "IAction.h"
+#include "action_process.h"
 
 using namespace BloombergLP;
 using namespace ProcessManager;
@@ -25,9 +29,34 @@ bool MessageConsumer::processMessage(const rmqt::Message& message)
             std::cout << "Currrent Thread id: " << std::this_thread::get_id() << std::endl;
             std::cout << "Test data: " << data << std::endl;
             bsl::shared_ptr<config> conf = bsl::make_shared<config>();
+            bsl::vector<bsl::string> responseVec;
             if(conf->readData(data))
             {
                 boost::process::filesystem::path process = boost::process::search_path(conf->get("Process").c_str());
+                if(boost::filesystem::exists(process))
+                {
+                    bsl::shared_ptr<IAction> action = bsl::make_shared<action_process>(process.generic_string(), conf->get("Param").c_str());
+                    action->execute();
+                    responseVec = action->getResponse();
+                }
+                else
+                {
+                    return bsl::string("ERROR: Could not find the process file at: ") + process.generic_string();
+                }
+            }
+            else
+            {
+                return bsl::string("ERROR: Malformed data, need JSON format: ") + data;
+            }
+
+            if(responseVec.size() != 0)
+            {
+                bsl::string response;
+                for(auto resp : responseVec)
+                {
+                    response.append(resp);
+                }
+                return response;
             }
             
             return bsl::string("Success");
